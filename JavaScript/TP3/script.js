e9d945facf321d567d4da32ff647a97a2307919c
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-05-02 19:40:05 +0100

    fix a mistak

--- JavaScript/TP3/script.js
+++ JavaScript/TP3/script.js
@@ -11,9 +11,9 @@ function verifierQuiz() {
         q5: "object"
     };
 
-    // Recupere les reponses selectionnées par l'utilisateur
-    const questions = document.querySelectorAll('.question');
-    const resultat = document.getElementById("resultat");
+    // Recupere les reponses selectionnées par l'utilisateur avec jQuery
+    const questions = $(".question");
+    const resultat = $("#resultat");
 
     // Verifie les reponses de l'utilisateur
     let score = calculerScore(questions, bonnesReponses);
@@ -23,13 +23,19 @@ function verifierQuiz() {
         return alert("Vous n'avez pas repondu a toutes les questions !");
     }
 
-    // Affiche le resultat du quiz
-    resultat.innerText = afficherResultat(score, questions.length)
+    // Affiche le resultat du quiz avec jQuery
+    resultat.text(afficherResultat(score, questions.length));
 }
+
+// Evenement Click avec jQuery
+$("button").click(function () { 
+    verifierQuiz();
+});
+
 
 function toutesRepondues(question){
     for (let i = 0; i < question.length; i++) {
-        reponse = question[i].querySelector('input:checked');
+        reponse = question[i].querySelector("input:checked");
         if (!reponse){
             return false;
         }
@@ -37,10 +43,11 @@ function toutesRepondues(question){
     return true;
 }
 
+
 function calculerScore(question, bonnesReponses){
     let score = 0;
     for (let i = 0; i < question.length; i++) {
-        const reponses = question[i].querySelectorAll('input');
+        const reponses = question[i].querySelectorAll("input");
         reponses.forEach(reponse => {
             if (reponse.checked){
                 if(reponse.value == bonnesReponses[reponse.name]) score++;
